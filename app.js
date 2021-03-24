1c817d11c9b904c09645ab29eda26a79f811a342
Danmoits <772523546@qq.com>
2021-03-24 22:21:45 +0800

    Ninth Commit, can really send dynamics

--- app.js
+++ app.js
@@ -1,67 +1,82 @@
 const http = require('http');
 const fs = require('fs');
 const url = require('url');
+let template = require('art-template');
+let comments = [];
 http
-    .createServer(function (req, res) {
-        let parseObj = url.parse(req.url, true);
-        let pathname = parseObj.pathname;
-        if(pathname === '/') {
-            fs.readFile('./views/index.html', function (err, data) {
-                if(err) {
-                    return res.end('404 Not Found.')
-                }
-                res.end(data);
-            });
+  .createServer(function (req, res) {
+    let parseObj = url.parse(req.url, true);
+    let pathname = parseObj.pathname;
+    if (pathname === '/') {
+      fs.readFile('./views/index.html', function (err, data) {
+        if (err) {
+          return res.end('404 Not Found.')
         }
-        else if(pathname === '/found') {
-            fs.readFile('./views/found.html', function (err, data) {
-                if (err) {
-                  return res.end('404 Not Found.')
-                }
-                res.end(data)
-              });
-        } 
-        else if(pathname === '/user_1') {
-            fs.readFile('./views/user_1.html', function (err, data) {
-                if(err) {
-                    return res.end('404 Not Found.')
-                }
-                res.end(data);
-            });
+        let htmlStr = template.render(data.toString(), {
+          comments: comments
+        })
+        res.end(htmlStr);
+      });
+    }
+    else if (pathname === '/found') {
+      fs.readFile('./views/found.html', function (err, data) {
+        if (err) {
+          return res.end('404 Not Found.')
         }
-        else if(pathname === '/user_2') {
-            fs.readFile('./views/user_2.html', function (err, data) {
-                if(err) {
-                    return res.end('404 Not Found.')
-                }
-                res.end(data);
-            });
+        res.end(data)
+      });
+    }
+    else if (pathname === '/user_1') {
+      fs.readFile('./views/user_1.html', function (err, data) {
+        if (err) {
+          return res.end('404 Not Found.')
         }
-        else if(pathname === '/user1_artical1') {
-            fs.readFile('./views/user1_artical1.html', function (err, data) {
-                if(err) {
-                    return res.end('404 Not Found.')
-                }
-                res.end(data);
-            });
+        res.end(data);
+      });
+    }
+    else if (pathname === '/user_2') {
+      fs.readFile('./views/user_2.html', function (err, data) {
+        if (err) {
+          return res.end('404 Not Found.')
         }
-        else if(pathname === '/user1_artical2') {
-            fs.readFile('./views/user1_artical2.html', function (err, data) {
-                if(err) {
-                    return res.end('404 Not Found.')
-                }
-                res.end(data);
-            });
+        res.end(data);
+      });
+    }
+    else if (pathname === '/user1_artical1') {
+      fs.readFile('./views/user1_artical1.html', function (err, data) {
+        if (err) {
+          return res.end('404 Not Found.')
         }
-        else if(pathname.indexOf('/public/') === 0) {
-            fs.readFile('.' + pathname, function (err, data) {
-                if (err) {
-                  return res.end('404 Not Found.')
-                }
-                res.end(data)
-              })
+        res.end(data);
+      });
+    }
+    else if (pathname === '/user1_artical2') {
+      fs.readFile('./views/user1_artical2.html', function (err, data) {
+        if (err) {
+          return res.end('404 Not Found.')
         }
-    })
-    .listen(3000, function () {
-        console.log('服务器启动成功，可以通过 http://127.0.0.1:3000/ 来进行访问');
-    })
+        res.end(data);
+      });
+    }
+    else if (pathname.indexOf('/public/') === 0) {
+      fs.readFile('.' + pathname, function (err, data) {
+        if (err) {
+          return res.end('404 Not Found.')
+        }
+        res.end(data)
+      })
+    }
+    else if(pathname === '/dynamic'){
+      let dynamic = parseObj.query;
+      comments.unshift(dynamic);
+      res.statusCode = 302;
+      res.setHeader('Location', '/');
+      res.end();
+    }
+    else {
+      return res.end('404 Not Found.');
+    }
+  })
+  .listen(3000, function () {
+    console.log('服务器启动成功，可以通过 http://127.0.0.1:3000/ 来进行访问');
+  })
--- public/js/index.js
+++ public/js/index.js
@@ -2,48 +2,48 @@
 const toTop = document.getElementsByClassName("to-top")[0];
 let scrollTop = parseInt(document.documentElement.scrollTop);
 let timer_toTop = setInterval(() => {
-    scrollTop = parseInt(document.documentElement.scrollTop);
-    if (scrollTop > 600) {
-        toTop.style.visibility = "visible";
-    }
-    else {
-        toTop.style.visibility = "hidden"
-    }
+  scrollTop = parseInt(document.documentElement.scrollTop);
+  if (scrollTop > 600) {
+    toTop.style.visibility = "visible";
+  }
+  else {
+    toTop.style.visibility = "hidden"
+  }
 }, 100);
 
 toTop.onclick = function () {
-    document.documentElement.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
 
 // 侧边栏
 const user = document.getElementsByClassName("user")[1];
 const creatorCenter = document.getElementsByClassName("creator-center")[1];
 let timer_sideBar = setInterval(() => {
-    scrollTop = parseInt(document.documentElement.scrollTop);
-    if (scrollTop > 1700) {
-        user.style.display = "block";
-        creatorCenter.style.display = "block";
-    }
-    else {
-        user.style.display = "none";
-        creatorCenter.style.display = "none";
-    }
+  scrollTop = parseInt(document.documentElement.scrollTop);
+  if (scrollTop > 1700) {
+    user.style.display = "block";
+    creatorCenter.style.display = "block";
+  }
+  else {
+    user.style.display = "none";
+    creatorCenter.style.display = "none";
+  }
 }, 100);
 
 // 点击喜欢心变红，再点击变灰
 const HeartArr = document.getElementsByClassName("icon_heart");
 let check_heart = 0;
 for (let i = 0; i < HeartArr.length; i++) {
-    HeartArr[i].onclick = function () {
-        if (check_heart === 0) {
-            this.style.color = "#E10E05";
-            check_heart = 1;
-        }
-        else {
-            this.style.color = "#888"
-            check_heart = 0;
-        }
+  HeartArr[i].onclick = function () {
+    if (check_heart === 0) {
+      this.style.color = "#E10E05";
+      check_heart = 1;
     }
+    else {
+      this.style.color = "#888"
+      check_heart = 0;
+    }
+  }
 }
 
 // 点击评论出现评论框，带过渡效果
@@ -53,33 +53,33 @@ const cmtCttArr = document.getElementsByClassName("comment-content");
 const cmtBodyArr = document.getElementsByClassName("comment-body");
 let check_rmk = 0;
 for (let i = 0; i < rmkBtnArr.length; i++) {
-    rmkBtnArr[i].onclick = function () {
-        // 之前想错，把remark的position改为absolute，反而麻烦不少
-        if (check_rmk === 0) {
-            check_rmk = 1;
-            rmkArr[i].className = "remark chosen";
-        }
-        else {
-            check_rmk = 0;
-            rmkArr[i].className = "remark not-chosen";
-        }
+  rmkBtnArr[i].onclick = function () {
+    // 之前想错，把remark的position改为absolute，反而麻烦不少
+    if (check_rmk === 0) {
+      check_rmk = 1;
+      rmkArr[i].className = "remark chosen";
     }
+    else {
+      check_rmk = 0;
+      rmkArr[i].className = "remark not-chosen";
+    }
+  }
 }
 
 //点击图片放大，再点一次缩小
 const imgArr = document.getElementsByClassName("comment-img");
 let check_zoom = 0;
 for (let i = 0; i < imgArr.length; i++) {
-    imgArr[i].onclick = function () {
-        if(check_zoom === 0){
-            this.style.width = "500px";
-            check_zoom = 1;
-        }
-        else{
-            this.style.width = "164px";
-            check_zoom = 0;
-        }
+  imgArr[i].onclick = function () {
+    if (check_zoom === 0) {
+      this.style.width = "500px";
+      check_zoom = 1;
     }
+    else {
+      this.style.width = "164px";
+      check_zoom = 0;
+    }
+  }
 }
 
 //发动态
@@ -90,19 +90,19 @@ const myHead = document.getElementsByClassName("my-head")[0];
 const mask = document.getElementsByClassName("mask")[0];
 const pageMask = document.getElementsByClassName("page-mask")[0];
 
-sendBtn.onclick = function() {
-    send.style.height = "515px";
-    myHead.style.width = "64px";
-    myHead.style.height = "64px";
-    mask.style.height = "113px";
-    pageMask.className = "page-mask show";
+sendBtn.onclick = function () {
+  send.style.height = "515px";
+  myHead.style.width = "64px";
+  myHead.style.height = "64px";
+  mask.style.height = "113px";
+  pageMask.className = "page-mask show";
 }
-cancelBtn.onclick = function() {
-    send.style.height = "0";
-    myHead.style.width = "110px";
-    myHead.style.height = "110px";
-    setTimeout(() => {
-        mask.style.height = "110px";
-        pageMask.className = "page-mask";
-    }, 300);
+cancelBtn.onclick = function () {
+  send.style.height = "0";
+  myHead.style.width = "110px";
+  myHead.style.height = "110px";
+  setTimeout(() => {
+    mask.style.height = "110px";
+    pageMask.className = "page-mask";
+  }, 300);
 }
