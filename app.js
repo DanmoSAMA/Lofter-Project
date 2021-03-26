ba270f756a8b3406953ac9a254addf56eb197bc8
Danmoits <772523546@qq.com>
2021-03-26 12:06:44 +0800

    Eleventh Commit, update simple login system

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const fs = require('fs');
 const url = require('url');
 const mongoose = require('mongoose');
 const template = require('art-template');
-// const { title } = require('process');
 
 //操作数据库
 mongoose.connect("mongodb://127.0.0.1/Lofter-Project");
@@ -13,6 +12,7 @@ mongoose.connection.once("open", function () {
   console.log("数据库连接成功");
 });
 
+//发动态
 const Schema = mongoose.Schema;
 const dynSchema = new Schema({
   title: {
@@ -21,14 +21,21 @@ const dynSchema = new Schema({
   },
   message: String
 });
-let DynModel = mongoose.model("dynamics", dynSchema);
+const DynModel = mongoose.model("dynamics", dynSchema);
 
 let comments = [];
 
+//登录信息
+const LgSchema = new Schema({
+  id: String,
+  password: String
+});
+const LgModel = mongoose.model("loginInfo", LgSchema);
+
 //读取数据库中的数据，并加入comments数组中
-DynModel.find({},function (err, docs) {
-  if(!err) {
-    for(let i = docs.length - 1; i >= 0; i--){
+DynModel.find({}, function (err, docs) {
+  if (!err) {
+    for (let i = docs.length - 1; i >= 0; i--) {
       comments.push(docs[i]);
     }
   }
@@ -101,7 +108,6 @@ http
     else if (pathname === '/dynamic') {
       let dynamic = parseObj.query;
       comments.unshift(dynamic);
-      console.log(dynamic.title, dynamic.message);
 
       DynModel.create({
         title: dynamic.title,
@@ -124,6 +130,89 @@ http
         }
         res.end(data);
       });
+    }
+    else if (pathname === '/_login') {
+      //登录的思路：第一次登录即为注册，将id和password保存在数据库中
+      //下次登录的时候通过数据库里的id来检查密码，如果密码正确，才会登录成功，跳转页面
+      let _login = parseObj.query;
+
+      /*
+        if (LgModel.findOne({ id: _login.id }, function () { 
+          console.log(LgModel.findOne({ id: _login.id }, function () { })._conditions.id);
+        })._conditions.id === _login.id) {
+          console.log('已存在该用户和密码');
+        } else {
+          console.log('新用户');
+          LgModel.create({
+            id: _login.id,
+            password: _login.password
+          }, function (err) {
+            if (!err) {
+              console.log(`插入成功，id是"${_login.id}",password是"${_login.password}"`);
+            }
+          })
+        }
+
+        LgModel.create({
+          id: _login.id,
+          password: _login.password
+        }, function (err) {
+          if (!err) {
+            console.log(`插入成功，id是"${_login.id}",password是"${_login.password}"`);
+          }
+        })
+      */
+
+      LgModel.findOne({ id: _login.id },
+        function (err, data) {
+          if (!err) {
+            if (data !== null) {
+            //如果找到这个id，判断密码是否正确，正确则跳转到主页
+              if (data.password !== _login.password) {
+                console.log('密码错误');
+              }
+              else {
+                console.log('登录成功');
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+                res.end();
+              }
+            }
+            else {
+              //如果集合里有文档，就不插入，否则就插入(上边的方式不成功)
+              //下边这个方法能成功，不过似乎不需要这样用了，会报错 cannot read property 'password' of null
+
+              /*
+                LgModel.updateOne({
+                  id: _login.id,
+                  password: _login.password
+                }, {
+                  id: _login.id,
+                  password: _login.password
+                }, {
+                  upsert: true
+                }, function (err) {
+                  if (!err) {
+                    console.log(`插入成功，id是"${_login.id}",password是"${_login.password}"`);
+                    res.statusCode = 302;
+                    res.setHeader('Location', '/');
+                    res.end();
+                  }
+                });
+              */
+             
+              LgModel.create({
+                id: _login.id,
+                password: _login.password
+              }, function (err, data) {
+                console.log(`插入成功，id是"${_login.id}",password是"${_login.password}"`);
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+                res.end();
+              });
+            }
+          }
+        })
     }
     else {
       return res.end('404 Not Found.');
--- public/js/login.js
+++ public/js/login.js
@@ -1,14 +1,26 @@
+//改变背景图片
 const body = document.body;
-const imgArr = ['../img/login01.jpg','../img/login02.jpg','../img/login03.jpg','../img/login04.jpg'];
-
-// body.style.backgroundImage = 'url(' + JSON.parse(imgArr[Math.floor(Math.random()*imgArr.length)]) + ')';
-// body.style.backgroundImage = 'url(' + imgArr[Math.floor(Math.random()*imgArr.length)] + ')';
-// body.style.backgroundImage = ('url(' + imgArr[Math.floor(Math.random()*imgArr.length)] + ')').replace(reg1, '').replace(reg2, '');
-
-// console.log(imgArr[Math.floor(Math.random()*imgArr.length)]);
-
-body.style.backgroundImage = `url(${imgArr[Math.floor(Math.random()*imgArr.length)]})`;
-
-// console.log(`"url('${imgArr[Math.floor(Math.random()*imgArr.length)]}')"`);
-
+const imgArr = ['public/img/login01.jpg', 'public/img/login03.jpg', 'public/img/login04.jpg','public/img/login05.jpg',
+'public/img/login07.jpg','public/img/login08.jpg','public/img/login09.jpg','public/img/login12.png'];
+body.style.backgroundImage = `url(${imgArr[Math.floor(Math.random() * imgArr.length)]})`;
 console.log(body.style.backgroundImage);
+
+//不选中checkbox无法提交表单
+const submitBtn = document.getElementsByClassName("submit-btn")[0];
+const checkBox = document.getElementsByClassName("checkbox")[0];
+const notChecked = document.getElementsByClassName("not-checked")[0];
+const id = document.getElementsByTagName("form")[0].getElementsByTagName("input")[0];
+const password = document.getElementsByTagName("form")[0].getElementsByTagName("input")[1];
+
+let timer;
+submitBtn.onclick = function () {
+  if (!checkBox.checked && id.value.length >= 3 && password.value.length >= 6) {
+    clearTimeout(timer);
+    notChecked.className = "not-checked touched";
+    timer = setTimeout(function() {
+      notChecked.className = "not-checked";
+    },2000)
+    return false;
+  }
+  return true;
+};
